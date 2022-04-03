5b8b54b9f7b058a8f26cdfde0aaa53cd40f2794d
Max <mvinais@mgen.fr>
2022-04-03 11:30:56 +0200

    Cellar Scene

--- src/Scenes/CellarScene.ts
+++ src/Scenes/CellarScene.ts
@@ -18,20 +18,13 @@ export default class CellarScene extends GameScene {
         super('Cellar');
 
         this.levelObjects = [
-            new LevelObject('bedroom', 'bedroom', 0, 0, false, 1),
-            new LevelObject('bedroomDresser_A', 'bedroom-dresser', 990, 313, true),
-            new LevelObject('bedroomDresser_B', 'bedroom-dresser', 840, 313, true),
-            new LevelObject('bedroomTable_A', 'bedroom-table', 836, 562, true),
-            new LevelObject('bedroomTable_B', 'bedroom-table', 935, 562, true),
-            new LevelObject('bed_A', 'bed', 982, 473, false, 5),
-            new LevelObject('bed_B', 'bed', 735, 473, false, 5),
-            new LevelObject('double_carton', 'double_carton', 250, 570, true),
-            new LevelObject('carton_A', 'carton', 200, 400, true),
-            new LevelObject('carton_B', 'carton', 400, 490, true),
-            new LevelObject('tv', 'tv', 500, 50, true),
-            new LevelObject('tapis', 'tapis', 465, 200, false, 2),
-            new LevelObject('chair_A', 'chair', 250, 80, true),
+            new LevelObject('cellar', 'cellar', 0, 0, false, 1),
         ];
+
+        for(let idx=0; idx < Math.random() * (15 - 7) + 7; idx++){
+            const carton: LevelObject = new LevelObject('carton_'+idx, 'carton',  Math.random() * (1060 - 75) + 75, Math.random() * (597 - 136) + 136, true);
+            this.levelObjects.push(carton);
+        }
 
         this.ambiantLight = 0x101010;
     }
@@ -42,7 +35,6 @@ export default class CellarScene extends GameScene {
 
     create():void {
         // ajout des méchants avant le super create pour que les méthodes de follow / game over etc soient prises en compte dans la classe parente
-        this.enemies.push(new Enemy(this, 580,400, 'north'));
         super.create();
         LevelObject.find("bedroom", this.levelObjects)?.objectRef.setInteractive().on('pointerdown', evt => console.log(`${evt.position.x} ; y : ${evt.position.y}`))
     }
