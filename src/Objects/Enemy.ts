b9db51a062b29e2d6956f780c2420390c7668c01
Quamiye <camille.candle@live.fr>
2022-04-03 10:15:34 +0200

    enemy anim begining

--- src/Objects/Enemy.ts
+++ src/Objects/Enemy.ts
@@ -7,23 +7,51 @@ export class Enemy extends Physics.Arcade.Sprite {
     vision : Vision;
 
     constructor(scene : Scene, x, y, dir : string){
-        super(scene, x, y, 'baddy');
+        super(scene, x, y, 'enemy_sprites', 58);
         scene.add.existing(this);
         scene.physics.add.existing(this);
         this.body.setCircle(50);
 
 
         this.vision = new Vision(scene, this.x, this.y, dir);
-        if(dir == "north"){
-            this.angle =  180;           
-        }
-        else if(dir == "west"){
-            this.angle = 90;
+        // if(dir == "north"){
+        //     this.angle =  180;           
+        // }
+        // else if(dir == "west"){
+        //     this.angle = 90;
 
-        }
-        else if(dir == "east"){
-            this.angle = 270;
-        }
+        // }
+        // else if(dir == "east"){
+        //     this.angle = 270;
+        // }
+
+        this.anims.create({
+            key:"left",
+            frames: this.anims.generateFrameNumbers('enemy_sprites', { start: 69, end: 71 }),
+            frameRate: 10,
+            repeat: -1
+        });
+
+        this.anims.create({
+            key:"right",
+            frames: this.anims.generateFrameNumbers('enemy_sprites', { start: 81, end: 83 }),
+            frameRate: 10,
+            repeat: -1
+        });
+
+        this.anims.create({
+            key:"up",
+            frames: this.anims.generateFrameNumbers('enemy_sprites', { start: 93, end: 95 }),
+            frameRate: 10,
+            repeat: -1
+        });
+
+        this.anims.create({
+            key:"down",
+            frames: this.anims.generateFrameNumbers('enemy_sprites', { start: 57, end: 59 }),
+            frameRate: 10,
+            repeat: -1
+        });
     }
 
     catSeen(){
