4b1d4cd7bb100720d2be9f8700b22b3411203d23
Max <mvinais@mgen.fr>
2022-04-03 09:23:40 +0200

    Remove log in text

--- src/Scenes/BedroomScene.ts
+++ src/Scenes/BedroomScene.ts
@@ -41,9 +41,7 @@ export default class BedroomScene extends GameScene {
         const mechant = new Enemy(this, 580,400, 'north');
         mechant.setDepth(10);
         mechant.vision.setDepth(10);
-        LevelObject.find("bedroom", this.levelObjects)?.objectRef.setInteractive().on('pointerdown', (evt) => {
-            this.ajouterTexte(`${evt.position.x} ; y : ${evt.position.y} Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum`, 'Alexouille', 80);
-        })
+        LevelObject.find("bedroom", this.levelObjects)?.objectRef.setInteractive().on('pointerdown', evt => console.log(`${evt.position.x} ; y : ${evt.position.y}`))
         this.physics.add.overlap(this.cat, mechant, () => mechant.catSeen(this.scene));
         this.physics.add.overlap(this.cat, mechant.vision, () => mechant.catSeen(this.scene));
     }
